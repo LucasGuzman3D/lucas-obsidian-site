8d429ba73d0024ae5d4ff3aa90c0580103691f17
lucasguzman3d <lucasguzman3d@gmail.com>
2026-03-09 08:50:56 +0100

    config: update baseUrl to lucasguzman.dev

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "lucasguzman3d.github.io/lucas-obsidian-site",
+    baseUrl: "lucasguzman.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
